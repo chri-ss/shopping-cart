31f9a4d6bad977b46c1c11d4549bd21cde21ae31
Chris Cresswell <Christorper@gmail.com>
2022-10-02 21:03:46 -0700

    Add nav links and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1 data-testid="app">hello</h1>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route />
+      </Routes>
+    </HashRouter>
   );
 };
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Shopping</li>
+        <li>Cart</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Home;
